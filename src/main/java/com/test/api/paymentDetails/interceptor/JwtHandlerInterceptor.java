9e23585bf9762c07f36a33c46cc05c6e243e3b6e
shrawanKeshari <shrawankeshari19@gmail.com>
2018-11-05 22:18:36 +0530

    jwt token authentication added in interceptor

--- src/main/java/com/test/api/paymentDetails/interceptor/JwtHandlerInterceptor.java
+++ src/main/java/com/test/api/paymentDetails/interceptor/JwtHandlerInterceptor.java
@@ -1,6 +1,8 @@
 package com.test.api.paymentDetails.interceptor;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,20 +12,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.test.api.paymentDetails.helpers.JsonHelper;
 import com.test.api.paymentDetails.pojo.PaymentResponse;
 
 @Component
 public class JwtHandlerInterceptor implements HandlerInterceptor {
 
-//	private static final String jwtTimestamp = "ts";
-//	private static final String jwtClientId = "clientId";
-//	private static final String jwtSceretKey = "1234567898876543234569877";
-//	private final long jwtWindow = 1000 * 60 * 5;
+	private static final String jwtTimestamp = "ts";
+	private static final String jwtClientId = "clientId";
+	private static final String jwtSceretKey = "1234567898876543234569877";
+	private final long jwtWindow = 1000 * 60 * 5;
 	private static final String jwtHeader = "jwtToken";
-	
+
 	private static final Logger LOGGER = Logger.getLogger(JwtHandlerInterceptor.class);
-	
+
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		String jwtToken = request.getHeader(jwtHeader);
@@ -34,10 +41,14 @@ public class JwtHandlerInterceptor implements HandlerInterceptor {
 		System.out.println("Into the pre handler method");
 
 		try {
-			if (!jwtToken.equals("1")) {
-				errorType = HttpServletResponse.SC_FORBIDDEN;
-				responseMessage = "error";
-			}
+			String clientId = getClientIdAndVerifyToken(jwtToken);
+			validateClientId(clientId);
+		} catch (IllegalArgumentException e) {
+			errorType = HttpServletResponse.SC_FORBIDDEN;
+			responseMessage = e.getMessage();
+		} catch (UnsupportedEncodingException e) {
+			errorType = HttpServletResponse.SC_FORBIDDEN;
+			responseMessage = e.getMessage();
 		} catch (Exception e) {
 			errorType = HttpServletResponse.SC_FORBIDDEN;
 			responseMessage = e.getMessage();
@@ -45,10 +56,48 @@ public class JwtHandlerInterceptor implements HandlerInterceptor {
 
 		if (errorType != -1) {
 			errorResponse(String.valueOf(errorType), response, responseMessage);
+			LOGGER.info("Error in validating request " + request.getRequestURI() + " ErrorType= " + errorType
+					+ " Message " + responseMessage);
 			return false;
 		}
 
 		return true;
+	}
+
+	private void validateClientId(String clientId) {
+		LOGGER.info("going to verify client id");
+		if (clientId == null || clientId.equals("")) {
+			throw new IllegalArgumentException("client authentication failed");
+		} else if (!clientId.equals("a5516f104428408fb6051f833c9bb9e0")) {
+			throw new IllegalArgumentException("client authentication failed");
+		}
+		LOGGER.info("client authenticated successfully");
+	}
+
+	private String getClientIdAndVerifyToken(String jwtToken)
+			throws IllegalArgumentException, UnsupportedEncodingException {
+
+		LOGGER.info("going to verify jwt token");
+
+		String clientId = null;
+
+		Algorithm algorithm = Algorithm.HMAC256(jwtSceretKey);
+		JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+		DecodedJWT decodedJWT = jwtVerifier.verify(jwtToken);
+		Map<String, Claim> claimMap = decodedJWT.getClaims();
+		validateTimestamp(claimMap.get(jwtTimestamp).asString());
+		clientId = claimMap.get(jwtClientId).asString();
+
+		return clientId;
+	}
+
+	private void validateTimestamp(String timeStamp) throws IllegalArgumentException {
+		LOGGER.info("Timestamp value is : " + timeStamp);
+		if (timeStamp == null || timeStamp.equals("")) {
+			throw new IllegalArgumentException("invalid timestamp");
+		} else if (Math.abs(System.currentTimeMillis() - Long.parseLong(timeStamp)) > jwtWindow) {
+			throw new IllegalArgumentException("invalid timestamp");
+		}
 	}
 
 	private void errorResponse(String errorType, HttpServletResponse response, String responseMessage)
